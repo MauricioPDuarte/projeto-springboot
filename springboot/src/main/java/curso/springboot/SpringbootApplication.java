6bc7692f0a57e76f89197cee6d32a00d3918837e
Mauricio Desktop <prussdesign@gmail.com>
2019-09-04 22:46:13 -0300

    Alteração do Eclipse

--- springboot/src/main/java/curso/springboot/SpringbootApplication.java
+++ springboot/src/main/java/curso/springboot/SpringbootApplication.java
@@ -2,12 +2,6 @@ package curso.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 public class SpringbootApplication {
@@ -15,6 +9,7 @@ public class SpringbootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 		//Alterado do GitHub
+		//Alterado do eclipse
 
 	}
 
