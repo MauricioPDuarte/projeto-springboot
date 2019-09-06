34b65e280437223aad3c566c0b3ebe6f1391f15e
Mauricio Desktop <prussdesign@gmail.com>
2019-09-06 17:17:27 -0300

    Adicionado botão de download na página de perfil e alterado nome download

--- springboot/src/main/java/curso/springboot/controller/RelatorioController.java
+++ springboot/src/main/java/curso/springboot/controller/RelatorioController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +14,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import curso.springboot.model.Pessoa;
+import curso.springboot.repository.PessoaRepository;
 import curso.springboot.util.RelatorioUtil;
 import net.sf.jasperreports.engine.JRException;
 
@@ -22,19 +25,23 @@ public class RelatorioController {
 	@Autowired
 	private RelatorioUtil relatorioUtil;
 	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
 	@GetMapping("/downloadRelatorio/{pessoaid}")
 	public void downloadRelatorioPdfPorId(
 			@PathVariable("pessoaid") Long pessoaid,
 			HttpServletRequest request,
 			HttpServletResponse response) throws JRException, SQLException, IOException {
 
+		Optional<Pessoa> pessoa = pessoaRepository.findById(pessoaid);
 		Map<String, Object> param = new HashMap<>();
 		param.put("ID_PESSOA", pessoaid);
 		
 		byte[] pdf = relatorioUtil.gerarRelatorioPdf("PessoaRelatorio", param, request.getServletContext());
 		response.setContentLength(pdf.length);
 		response.setContentType("application/octet-stream");
-		response.setHeader("Content-Disposition", "attachment; filename=relatorio.pdf");
+		response.setHeader("Content-Disposition", "attachment; filename=Perfil_"+ pessoa.get().getNome() +".pdf");
 		response.getOutputStream().write(pdf);
 		
 	}
